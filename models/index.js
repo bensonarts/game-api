42270b8f03f8f52beb2fd8b08cb966bf8bfd69b9
Aaron Benson <bensonan55@gmail.com>
2019-08-25 17:55:23 -0500

    update docker

--- models/index.js
+++ models/index.js
@@ -11,13 +11,20 @@ const db = {};
 
 let sequelize;
 
+/*
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+ */
+sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+});
 
-sequelize = new Sequelize(config.database, config.username, config.password, config);
+// sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs
   .readdirSync(__dirname)
